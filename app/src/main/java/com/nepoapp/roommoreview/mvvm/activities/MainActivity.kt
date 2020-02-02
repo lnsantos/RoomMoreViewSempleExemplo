c4509fa521c05ca300d7a0f40ca107c238a17739
Lucas <lucas.nepomuceno1999@gmail.com>
2020-02-02 19:45:22 -0300

    configed recyclerview in main activity

--- app/src/main/java/com/nepoapp/roommoreview/mvvm/activities/MainActivity.kt
+++ app/src/main/java/com/nepoapp/roommoreview/mvvm/activities/MainActivity.kt
@@ -2,13 +2,31 @@ package com.nepoapp.roommoreview.mvvm.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.nepoapp.roommoreview.R
+import com.nepoapp.roommoreview.mvvm.adapter.WordRecyclerViewAdapter
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var recyclerViewWord : RecyclerView
+    private lateinit var adapterWord : WordRecyclerViewAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setupView()
+
+    }
+
+    private fun setupView() {
+        recyclerViewWord = recyclerview
+        adapterWord = WordRecyclerViewAdapter(this)
+
+        recyclerViewWord.adapter = adapterWord
+        recyclerViewWord.layoutManager = LinearLayoutManager(this)
     }
 
 }
--- app/src/main/java/com/nepoapp/roommoreview/mvvm/adapter/WordRecyclerViewAdapter.kt
+++ app/src/main/java/com/nepoapp/roommoreview/mvvm/adapter/WordRecyclerViewAdapter.kt
@@ -3,18 +3,34 @@ package com.nepoapp.roommoreview.mvvm.adapter
 import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.nepoapp.roommoreview.R
 import com.nepoapp.roommoreview.domain.Word
+import kotlinx.android.synthetic.main.recyclerview_items.view.*
 
-class WordRecyclerViewAdapter(ctx:Context) {
+class WordRecyclerViewAdapter(ctx:Context) :
+    RecyclerView.Adapter<WordRecyclerViewAdapter.WordViewHolder>() {
 
     private val inflater: LayoutInflater = LayoutInflater.from(ctx)
-    private var words = emptyList<Word>() // Cached copy of words
+    private var words = emptyList<Word>()
 
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): WordViewHolder
+        = WordViewHolder(inflater.inflate(R.layout.recyclerview_items,parent,false))
+
+    override fun getItemCount(): Int = words.size
+
+    override fun onBindViewHolder(holder: WordViewHolder, position: Int) {
+        holder.wordItemView.text = words[position].word
+    }
+
+    internal fun setWords(words: List<Word>) {
+        this.words = words
+        notifyDataSetChanged()
+    }
 
     inner class WordViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        val wordItemView: TextView = itemView.findViewById(R.id.textView)
+        val wordItemView: TextView = itemView.textView
     }
 }
